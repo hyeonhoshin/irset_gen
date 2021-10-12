f4d506bc09dedebe79b99b6a274a51020fdcfa01
Lab:Hyeonho <tlsgusghq@kaist.ac.kr>
2021-10-12 23:50:56 +0900

    FPS 기록이 이상한것 빼고는 기록 속도는 충분히 빨라짐.

--- webcam.py
+++ webcam.py
@@ -1,46 +1,62 @@
 # -*-coding: utf-8 -*-
 # 이 코드는 두 카메라를 동시에 동시에 사용가능함을 확인함.
+from __future__ import print_function
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
+import argparse
+import imutils
 import cv2
 import time
 import numpy as np
 
-# cap 이 정상적으로 open이 되었는지 확인하기 위해서 cap.isOpen() 으로 확인가능
-cap = cv2.VideoCapture(0)
+from FPS import FPS
+from WebcamVideoStream import WebcamVideoStream
 
-# cap.get(prodId)/cap.set(propId, value)을 통해서 속성 변경이 가능.
-# 3은 width, 4는 heigh
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+    help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+    help="Whether or not frames should be displayed")
+# ap.add_argument('--folder', type=str, default='test', help="Where to save?")
+args = vars(ap.parse_args())
 
-print('width: {0}, height: {1}'.format(cap.get(3),cap.get(4)))
-cap.set(3,360)
-cap.set(4,640)
+# Generate folder
+# import os
+# try:
+#     os.makedirs(args.folder, exist_ok=True)
+# except:
+#     print("Cannot make directory!")
+#     exit(-1)
+# print("Directory is made.")
 
-
-start_time = time.time()
-monitor_time = 3
+# created a *threaded* video stream, allow the camera sensor to warmup,
+# and start the FPS counter
+print("[INFO] sampling THREADED frames from webcam...")
+vs = WebcamVideoStream(src=0).start()
+fps = FPS().start()
 
 buffer = []
+# loop over some frames...this time using the threaded stream
+while fps._numFrames < args["num_frames"]:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    buffer.append(frame)
+    # check to see if the frame should be displayed to our screen
+    if args["display"] > 0:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+    # update the FPS counter
+    fps.update()
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-while(time.time()-start_time < monitor_time):
-    # ret : frame capture결과(boolean)
-    # frame : Capture한 frame
-    ret, frame = cap.read() 
-
-    if (ret):
-        # image를 Grayscale로 Convert함.
-        buffer.append(frame)
-
-        # cv2.imshow('frame', frame)
-        
-
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-            # break
-
-# Post saving
 for i, img in enumerate(buffer):
-    cvt = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    f = open(f"{i:05}.npy",'wb')
-    np.save(f,np.array(cvt))
-    f.close()
+    cv2.imwrite(f'test/{i:05}.png', img)
 
-# cap.release()
-# cv2.destroyAllWindows()
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
