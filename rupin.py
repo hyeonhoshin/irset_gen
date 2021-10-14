80f0808307d8dca87476e52e5420f9b43aa023c3
Lab:Hyeonho <tlsgusghq@kaist.ac.kr>
2021-10-14 23:02:35 +0900

    Bytes converting error is debugged.

--- rupin.py
+++ rupin.py
@@ -50,25 +50,22 @@ if(ser.is_open):
 def s16(val):
     return -(val & 0x8000)|(val&0x7fff)
 
-try:
-    for i in range(args.count):
-        buffer = []
-        ser.write(b'reg read 0x10 0x78\n')
-        frame = ser.read_until()
 
-        for j in range(0,360,6):
-            prior = frame[j:j+2]
-            post = frame[j+3:j+5]
-            buffer.append(s16(int(prior+post)))
+for i in range(args.count):
+    buffer = []
+    ser.write(b'reg read 0x10 0x78\n')
+    frame = ser.read_until()
 
-        tmp = np.array(buffer)
-        f = open(args.folder+f'/{i:05}.npy', 'wb')
-        np.save(f,tmp)
-        f.close()
-except:
-    print('Read error!')
-    exit(-1)
+    for j in range(0,360,6):
+        prior = frame[j:j+2]
+        post = frame[j+3:j+5]
+        # buffer.append(s16(int(prior+post)))
+        buffer.append(int.from_bytes(prior+post, byteorder='big', signed=True))
 
+    tmp = np.array(buffer)
+    f = open(args.folder+f'/{i:05}.npy', 'wb')
+    np.save(f,tmp)
+    f.close()
 
 
 # output : /folder_name/"%05.npy" per frame.
